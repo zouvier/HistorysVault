7d509ba45ed5ed120977c3275b8c062934a0bc3e
Zoumana Cisse <zoumanacisse692@gmail.com>
2021-09-04 21:50:27 -0700

    began Historys Vault ERC721 contract

---
+++ contracts/HistorysVault.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract HistorysVault is ERC721 {
+    
+    ERC721(_name = 'Historys Vault');
+
+    
+}
